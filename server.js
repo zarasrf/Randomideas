11bf34378fff9c9b2610447eda3982f6018fcf61
zara sarafraz <zarasrf0@gmail.com>
2023-08-13 20:13:22 +0330

    commit till fetch api

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path =require('path')
 const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
 const port =process.env.PORT || 5000
 const connectDB = require('./config/db.js')
@@ -14,6 +15,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: false }))
+
+// cors middleware
+app.use(cors({
+    orgin: ['http://localhost:5000', 'http://localhost:3000'],
+    credentials:true
+}))
+
 
 app.get('/', (req, res) => {
     res.json({message: 'Welcome to the randomideas API'})
