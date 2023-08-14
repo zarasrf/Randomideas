12aaa5a8f0984c99ede78c8c84e92e33cf31e543
zara sarafraz <zarasrf0@gmail.com>
2023-08-14 16:14:04 +0330

    changes before deploying

--- client/src/services/ideasApi.js
+++ client/src/services/ideasApi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 class IdeasApi {
     constructor() {
-        this._apiUrl = 'http://localhost:5000/api/ideas'
+        this._apiUrl = '/api/ideas'
     }
     getIdeas() {
         return axios.get(this._apiUrl)
